166768cd5cd008c3dcd390f84f4babca32fb6926
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-05-14 07:56:47 +0600

    object in datatype is done

--- 01-basic/02-datatype.js
+++ 01-basic/02-datatype.js
@@ -1,9 +1,9 @@
 // Primitive data type
-    // String	represents sequence of characters e.g. "hello"
-    // Number	represents numeric values e.g. 100
-    // Boolean	represents boolean value either false or true
+    // String	    represents sequence of characters e.g. "hello"
+    // Number	    represents numeric values e.g. 100
+    // Boolean	    represents boolean value either false or true
     // Undefined	represents undefined value
-    // Null	represents null i.e. no value at all
+    // Null	        represents null i.e. no value at all
 
 var myString = "this a string data type"
 console.log(myString)
@@ -19,15 +19,16 @@ console.log(myUndefined)
 
 var myNull = null
 console.log(myNull)
-console.log("=====================")
-
 
 // Non-primitive (reference) data type
     // Object	represents instance through which we can access members
     // Array	represents group of similar values
     // RegExp	represents regular expression
-    // function a block of code designed to perform a particular task
+    // function represents a block of code designed to perform a particular task
 
+// By object literal
+console.log("==============By object literal=============");
+var methodName = "setprops"    
 let myObj = {
      name: "Toyoto", 
      model: 500, 
@@ -38,7 +39,8 @@ let myObj = {
          const { name, model, weight } = this
          console.log(`name = ${name}, model = ${model}, weight = ${weight}`)
      },
-     setProp: function(color, height)
+
+     [ methodName ]: function(color, height)
      {
         this.color = color
         this.height = height
@@ -47,6 +49,29 @@ let myObj = {
 console.log(myObj)
 console.log(myObj.name)
 myObj.details()
-myObj.setProp("red", 34)
+myObj.setprops("red", 56)
 console.log(myObj);
 
+// By creating instance of Object
+console.log("=============By creating instance of Object==============");
+var myInstance = new Object()
+myInstance.id = 054003
+myInstance.name = "Jakir"
+myInstance.salary = 50000
+console.log(myInstance);
+
+// By using an Object constructor
+console.log("============By using an Object constructor==========");
+function myConstrutor(first, last, age) {
+    this.firstName = first
+    this.lastName = last
+    this.age = age
+}
+// create a object
+var myConstrutor1 = new myConstrutor("Jakir", "Ahmed", 35)
+console.log(myConstrutor1)
+
+myConstrutor1.name = function(){
+    return this.firstName + " " + this.lastName
+}
+console.log(myConstrutor1.name());
