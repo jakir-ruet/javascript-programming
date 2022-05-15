57756c0b101859af7ad4234f279947e16e8bc83a
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-05-15 09:30:26 +0600

    prime checking done

--- 01-basic/02-control-statement/problem-solving/check-prime.js
+++ 01-basic/02-control-statement/problem-solving/check-prime.js
@@ -1,31 +1,40 @@
-function PrimeNonprime(num)
-{
-    if (num < 0) 
-    {
-        console.log(`the number is negative ${num}`);
-    }
-    else if(Math.trunc(num) !== num)
-    {
-        console.log(`the number is decimal ${num}`);
-    }
-    else
-    {
-        if (num === 0 || num === 1 ) 
+
+function primeNumberInterval(start, end) {
+    for (let i = start; i < end; i++) {
+    
+    let isPrime = true
+    if (i === 0 || i === 1)
         {
-            console.log(`the number neither prime nor composite ${num}`);
+            console.log(`0 & 1 is neither prime nor composite`)
+            continue
         }
-        else
+    else if(i < 0)
         {
-            for (let i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    console.log(`this composite ${num}`);
-                }
-                else{
-                    console.log(`prime ${num}`);
+            console.log(`${i} this is negative number`);
+        }
+    else if(Math.trunc(i) !== i)
+        {
+            console.log(`${i} this fractional number`);
+        }
+    else if(i > 1)
+        {
+            for (let j = 2; j < i; j++)
+            {
+                if(i % j == 0)
+                {
+                    isPrime = false
+                    break
                 }
             }
         }
+    if (isPrime)
+        {
+            console.log(`${i} is the prime number`);    
+        }
+    else
+        {
+            console.log(`${i} it is not prime number`);
+        }
     }
 }
-
-PrimeNonprime(9)
+primeNumberInterval(1, 15)
