9357397dc913e935d334ded702ef2467e4cc4de4
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-05-15 09:31:46 +0600

    prime checking done

--- 01-basic/02-control-statement/problem-solving/check-prime.js
+++ 01-basic/02-control-statement/problem-solving/check-prime.js
@@ -2,39 +2,39 @@
 function primeNumberInterval(start, end) {
     for (let i = start; i < end; i++) {
     
-    let isPrime = true
-    if (i === 0 || i === 1)
-        {
-            console.log(`0 & 1 is neither prime nor composite`)
-            continue
-        }
-    else if(i < 0)
-        {
-            console.log(`${i} this is negative number`);
-        }
-    else if(Math.trunc(i) !== i)
-        {
-            console.log(`${i} this fractional number`);
-        }
-    else if(i > 1)
-        {
-            for (let j = 2; j < i; j++)
+        let isPrime = true
+        if (i === 0 || i === 1)
             {
-                if(i % j == 0)
+                console.log(`0 & 1 is neither prime nor composite`)
+                continue
+            }
+        else if(i < 0)
+            {
+                console.log(`${i} this is negative number`);
+            }
+        else if(Math.trunc(i) !== i)
+            {
+                console.log(`${i} this fractional number`);
+            }
+        else if(i > 1)
+            {
+                for (let j = 2; j < i; j++)
                 {
-                    isPrime = false
-                    break
+                    if(i % j == 0)
+                    {
+                        isPrime = false
+                        break
+                    }
                 }
             }
-        }
-    if (isPrime)
-        {
-            console.log(`${i} is the prime number`);    
-        }
-    else
-        {
-            console.log(`${i} it is not prime number`);
-        }
+        if (isPrime)
+            {
+                console.log(`${i} is the prime number`);    
+            }
+        else
+            {
+                console.log(`${i} it is not prime number`);
+            }
     }
 }
 primeNumberInterval(1, 15)
