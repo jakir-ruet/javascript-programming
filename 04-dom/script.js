c68b5e2d0e46b7ee4a372dd81d613815cfac0e20
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-06-05 10:21:16 +0600

    dom in going on

--- 04-dom/script.js
+++ 04-dom/script.js
@@ -18,4 +18,24 @@ console.log(document.all[5])
 console.log(document.images)
 console.log(document.links)
 
-//============DOM Traversing============
+//============02-DOM Traversing >> getElementById============
+console.log(document.getElementById("nameId"))
+
+//change the name in div tag with specific id
+let divElement = document.getElementById('myName')
+
+divElement.textContent = "my name is Jakir Ahmed by textContent"
+console.log(divElement.textContent)
+divElement.innerText = "my name is Jahid Ahmed by innerText"
+console.log(divElement.innerText)
+divElement.innerHTML = "my name is Jabir Ahmed by innerHTML"
+console.log(divElement.innerHTML)
+
+//her divElement is a object not variable
+divElement.style.backgroundColor = "red"
+divElement.style.color = "white"
+divElement.style.padding = "10px"
+divElement.style.borderRadius = "5px"
+
+//============02-DOM Traversing >> getElementByClass============
+console.log(document.getElementsByClassName('item'))
