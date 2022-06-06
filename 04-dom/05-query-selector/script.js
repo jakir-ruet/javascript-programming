89d08cf4978deb8c71df53a105d3e7bdd5f20e8d
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-06-06 21:16:02 +0600

    query selector done

---
+++ 04-dom/05-query-selector/script.js
@@ -0,0 +1,20 @@
+let header = document.querySelector('.header')
+console.log(header)
+
+let firstItem = document.querySelector('.item:first-child')
+console.log(firstItem)
+firstItem.style.color = "red"
+
+let nItem = document.querySelector('.item:nth-child(2)')
+console.log(nItem)
+nItem.style.color = "green"
+
+let lastItem = document.querySelector('.item:last-child')
+console.log(lastItem)
+lastItem.style.backgroundColor = "red"
+
+let allItem = document.querySelectorAll('.item')
+console.log(allItem)
+
+
+
