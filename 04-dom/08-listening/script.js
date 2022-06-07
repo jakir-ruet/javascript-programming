de30817bc6fd3707342409acefc19992c0137ae7
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-06-07 22:47:35 +0600

    listening continue

---
+++ 04-dom/08-listening/script.js
@@ -0,0 +1,17 @@
+const myHead = document.querySelector('.myHeader')
+
+// myHead.addEventListener('click', () => {
+//    console.log(event)
+// })
+
+// myHead.addEventListener('mousedown', (event) => {
+//    console.log(event)
+// })
+
+// myHead.addEventListener('mouseup', (event) => {
+//    console.log(event)
+// })
+
+myHead.addEventListener('mouseenter', (event) => {
+   console.log(event)
+})
