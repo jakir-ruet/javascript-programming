e7261458c2e3443bee6b626f9b5952bb3bbc5679
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-07-11 17:46:38 +0600

    basic of JS Re-discuss

--- 01-basic/hello.js
+++ 01-basic/hello.js
@@ -1,8 +1,8 @@
 console.log("hello world")
 
 var myvar = "Hello from my var"
+var skill = ['HTML', 'CSS', 'JavaScript', 'NodeJS', 'Python']
 console.log(myvar)
-
 
 // string concatenation
 console.log(myvar + " hello concatenation " + 10)
@@ -11,3 +11,6 @@ console.log(myvar + " hello concatenation " + 10)
 console.log(`${myvar} hello string templating`)
 
 console.log(10+10)
+console.log(typeof(myvar));
+console.log(skill);
+console.log(typeof(skill))
