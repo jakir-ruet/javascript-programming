e30dbeaf15a9bb9e98dbcc2f78a72d1331a42d53
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-07-12 12:27:07 +0600

    first class function done

--- 05-exploration/01-function-first.js
+++ 05-exploration/01-function-first.js
@@ -13,7 +13,45 @@ let myArray = [4, 3, 5, 7, 2, 9];
 myArray.push(myFirstFunction);
 console.log(myArray);
 
-// 3-=======Assign a function in an object/property=======
+// 3-=======Assign a function as an object/property=======
+let myObject = {
+   name: 'Jakir',
+   designation: 'CEO',
+   address: myFirstFunction('This from another function'),
+   details: function(){
+      console.log(`Hi, function in object`);
+   }
+};
+console.log(myObject);
+
 // 4-=======Function create as we need=======
+let funcResult = 20 + function(){return 30}();
+console.log(`Function inline ${funcResult}`);
+
+function sumFunction(a, b) {
+   let sum = a + b;
+   return sum;
+}
+let sumResult = sumFunction(4, 5);
+console.log(`The sum result ${sumResult}`);
+
 // 5-=======Pass a function as an Argument=======
+function myNewFunction(name, myOldFunction) {
+   return myOldFunction(name);
+}
+let newFunction = myNewFunction('jakir new', myFirstFunction);
+console.log(`function as an argument ${newFunction}`);
+
 // 6-=======Return a function in a function=======
+function baseFunction(base) {
+   return function childFunction(child){
+      let childResult = 1;
+      for (let i = 0; i < base; i++) {
+         childResult *=  child;
+      }
+      return childResult;
+   }
+}
+let sqtResult = baseFunction(3);
+let finalResult = sqtResult(3);
+console.log(finalResult);
