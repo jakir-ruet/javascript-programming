a6f1791d0fba3c4beb28b4b41f0919026374787a
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-07-18 08:32:11 +0600

    data type added

--- 01-basic/04-data-type/datatype.js
+++ 01-basic/04-data-type/datatype.js
@@ -1,16 +1,3 @@
-// Primitive data type (non reference)
-    // String	    represents sequence of characters e.g. "hello"
-    // Number	    represents numeric values e.g. 100
-    // Boolean	    represents boolean value either false or true
-    // Undefined	represents undefined value
-    // Null	        represents null i.e. no value at all
-
-// Non-primitive (reference) data type
-    // Object	    represents instance through which we can access members
-    // Array	    represents group of similar values
-    // RegExp	    represents regular expression
-    // function     represents a block of code designed to perform a particular task
-
     let a = 20 + 30 + ' ' + 'hello world';
     let b = 'hello world' + ' ' + 20 + 30;
     console.log('======follow the order precedence======');
