787e0141a7324ea9515ed583c72410e381038ad8
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-07-18 10:02:31 +0600

    symbol in datatype added

---
+++ 01-basic/04-data-type/01-primitive/06-symbol.js
@@ -0,0 +1,21 @@
+let val1 = Symbol('this is symbol one');
+let val2 = Symbol('this is symbol two');
+console.log(val1 === val2);
+
+//typing checking
+console.log(typeof(val1));
+
+let id = Symbol('unique id');
+let person = {
+   name: 'John',
+   //adding symbol as a key
+   [id]: 100, //not 'id': 100;
+   age: 30
+}
+
+//Symbols are not included in for...in Loop
+//using for in
+for(let key in person){
+   //console.log(key);
+   //console.log(key, person);
+}
