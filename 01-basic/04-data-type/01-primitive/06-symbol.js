61a0252819498c5ff02bc51e23548f6e17b4dcb2
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-07-18 10:22:53 +0600

    datatype update

--- 01-basic/04-data-type/01-primitive/06-symbol.js
+++ 01-basic/04-data-type/01-primitive/06-symbol.js
@@ -1,11 +1,11 @@
 let val1 = Symbol('this is symbol one');
-let val2 = Symbol('this is symbol two');
+let val2 = Symbol('this is symbol one');
 console.log(val1 === val2);
 
 //typing checking
 console.log(typeof(val1));
 
-let id = Symbol('unique id');
+let id = Symbol('100');
 let person = {
    name: 'John',
    //adding symbol as a key
@@ -16,6 +16,6 @@ let person = {
 //Symbols are not included in for...in Loop
 //using for in
 for(let key in person){
-   //console.log(key);
+   console.log(key);
    //console.log(key, person);
 }
