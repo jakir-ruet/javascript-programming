2d724eafa1fec82332766fade4ac9e88e5362775
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-07-20 21:43:40 +0600

    big integer done

---
+++ 01-basic/04-data-type/01-primitive/07-bigint.js
@@ -0,0 +1,8 @@
+let bigInt = BigInt(889539485358358546);
+console.log(bigInt);
+
+let a = Number.MAX_SAFE_INTEGER;
+console.log(a);
+
+console.log(typeof bigInt);
+console.log(typeof BigInt(100) === 'bigint');
