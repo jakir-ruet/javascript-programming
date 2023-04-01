10765ef84c70d5e80c0a588e336b25768f188496
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-04-01 15:13:34 +0600

    slice done

---
+++ 07-es6/06-slice-array.js
@@ -0,0 +1,7 @@
+let myNums = [4, 3, 5, 6, 3, 8, 7, 9];
+let sliceNum = myNums.slice(3, 5);
+console.log(sliceNum);
+
+const lngs = ['JS', 'CSS', 'HTML', 'Go'];
+let newLngs = lngs.slice(0, -1);
+console.log(newLngs);
